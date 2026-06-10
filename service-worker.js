39f2842a6932f8dfb473bacca08b291b3f2de4c9
adam13131313 <adamshyde@proton.me>
2026-06-10 17:34:37 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE = "sydney-marathon-v7";
+var CACHE = "sydney-marathon-v8";
 var ASSETS = ["index.html", "manifest.json", "icon-192.png", "icon-512.png", "apple-touch-icon.png"];
 
 self.addEventListener("install", function(e) {
